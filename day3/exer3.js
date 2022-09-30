c5b7729c86aa2406e72c910acb826ec92f3e098d
Amol Chourasia <70088339+amolchourasia27@users.noreply.github.com>
2022-10-01 00:20:48 +0530

    Third pull hacktoberFest

---
+++ day3/exer3.js
@@ -0,0 +1,26 @@
+//1
+let firstName = 'Amol'
+let lastName = 'Chourasia'
+let country = 'India'
+let city = 'Bilaspur'
+let age  = 22
+let isMarried = false;
+let year = 2022
+
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(age))
+console.log(typeof(city))
+console.log(typeof(isMarried))
+console.log(typeof(year))
+//2
+let num = 10
+let textNum = '10'
+console.log(num === textNum) 
+//3
+let newInt = 9.8
+console.log(parseInt(num) === num);
+//4
+console.log('hello')
+console.log(null)
---
+++ day3/first3.js
@@ -0,0 +1,30 @@
+// let isRaining = true
+// isRaining
+//   ? console.log('You need a rain coat.')//executes when true
+//   : console.log('No need for a rain coat.')
+// isRaining = false
+
+// isRaining
+//   ? console.log('You need a rain coat.')
+//   : console.log('No need for a rain coat.')//executes when false
+
+
+//   prompt('required text', 'optional text')
+//   let number = prompt('Enter number', 'number goes here')
+// console.log(number)
+
+
+// const agree = confirm('Are you sure you like to delete? ')
+// console.log(agree) // result will be true or false based on what you click on the dialog box
+
+
+const now = new Date()
+console.log(now);
+
+console.log(now.getFullYear())
+
+console.log(now.getHours())
+
+console.log(now.getMonth()+1)
+
+console.log(now.getHours())
